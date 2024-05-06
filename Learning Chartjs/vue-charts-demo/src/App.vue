07970f1efc6537de98c21b4ba1e16b12b9620cdf
chnpngwng <107300257+chnpngwan@users.noreply.github.com>
2024-05-07 07:33:09 +0800

    update

---
+++ Learning Chartjs/vue-charts-demo/src/App.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import Bar from './components/Bar.vue';
+import Line from './components/Line.vue';
+import Pie from './components/Pie.vue';
+import Scatter from './components/Scatter.vue';
+import Bubble from './components/Bubble.vue';
+import Radar from './components/Radar.vue';
+import PolarArea from './components/PolarArea.vue';
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <h2>Bar Chart</h2>
+      <Bar id="bar" />
+    </div>
+    <div class="container">
+      <Pie id="pie" />
+    </div>
+    <div class="container">
+      <Line id="line" />
+    </div>
+    <div class="container">
+      <Scatter id="scatter" />
+    </div>
+    <div class="container">
+      <Bubble id="bubble" />
+    </div>
+    <div class="container">
+      <Radar id="radar" />
+    </div>
+    <div class="container">
+      <PolarArea id="polar" />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.container {
+  border-bottom: 2px solid;
+  height: 600px;
+  width: 90%;
+  margin: 0 auto;
+}
+
+h2 {
+  text-align: center;
+  padding: 1rem 0;
+}
+
+#bar,
+#pie,
+#line,
+#scatter,
+#bubble,
+#radar,
+#polar {
+  width: 300px;
+  max-height: 500px;
+  margin: 3rem 0;
+  padding: 2rem 0;
+}
+</style>
---
+++ Learning Chartjs/vue-charts-demo/src/components/Bar.vue
@@ -0,0 +1,49 @@
+<script lang="ts" setup>
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+import { Bar } from 'vue-chartjs'
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+
+const data = {
+  labels: [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ],
+  datasets: [
+    {
+      label: 'Data One',
+      backgroundColor: '#f87979',
+      data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
+    }
+  ]
+}
+
+const options = {
+  responsive: true,
+  maintainAspectRatio: true
+}
+
+
+</script>
+
+<template>
+  <Bar :data="data" :options="options" />
+</template>
